e131b37f5dc7d3a62f4141ef7bc06f7c303570be
xiaoyan <xiaoyan.beijing@qq.com>
2018-07-19 17:04:02 +0800

    1

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <div class="container">
-      <div class="sidebar">
-        <ul>
-          <li>
-            <router-link to="/">home</router-link>
-          </li>
-          <li>
-            <router-link to="/about">about</router-link>
-          </li>
-        </ul>
-      </div>
-      <div class="main">
-        <router-view></router-view>
+      <header class="navbar">nav</header>
+       <div class="body-box">
+          <div class="sidebar">
+            <ul>
+              <li>
+                <router-link to="/">home</router-link>
+              </li>
+              <li>
+                <router-link to="/about">about</router-link>
+              </li>
+            </ul>
+          </div>
+          <div class="main">
+            <router-view></router-view>
+          </div>
       </div>
     </div>
   </div>
@@ -29,19 +32,23 @@ export default {
 <style lang="scss">
 .home {
   .container {
-    display: flex;
-    justify-content: space-around;
     width: 100%;
+  }
+  .navbar{
+    height: 55px;
+    margin: 0;
+    padding: 0;
+    border-bottom: 1px solid #cfd8dc
   }
   .sidebar {
+    position: fixed;
     width: 200px;
-    height: 100vh;
-    border: 1px solid #ccc;
+    height: 100%;
+    overflow: scroll;
+    border-right:1px solid #ccc;
   }
-  .main {
-    width: 100%;
-    height: 100vh;
-    border: 1px solid #ccc;
+  .main{
+    margin-left: 200px;
   }
 }
 </style>
