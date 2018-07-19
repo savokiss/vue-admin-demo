1669276aab5c89d231c387910730b3d8d05b2baf
xiaoyan <xiaoyan.beijing@qq.com>
2018-07-19 17:33:32 +0800

    todo style

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'todomvc-app-css/index.css'
+// import 'todomvc-app-css/index.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
