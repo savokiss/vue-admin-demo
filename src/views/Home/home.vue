743074726d38d14397bec080ad2dc7f8d9acce1d
xiaoyan <xiaoyan.beijing@qq.com>
2018-07-24 16:58:52 +0800

    home page

--- src/views/Home/home.vue
+++ src/views/Home/home.vue
@@ -1,7 +1,34 @@
 <template>
   <div class="home">
-    <h1>This is an home page</h1>
+    <waterfall :line-gap="200" :watch="items">
+  <!-- each component is wrapped by a waterfall slot -->
+  <waterfall-slot
+    v-for="(item, index) in items"
+    :width="item.width"
+    :height="item.height"
+    :order="index"
+    :key="item.id"
+  >
+    <!--
+      your component
+    -->
+  </waterfall-slot>
+</waterfall>
   </div>
 </template>
 <script>
+import Waterfall from 'vue-waterfall/lib/waterfall'
+import WaterfallSlot from 'vue-waterfall/lib/waterfall-slot'
+export default {
+  name: 'home',
+  data (){
+    return {
+      items: ['1', 'a']
+    }
+  },
+  components: {
+    Waterfall,
+    WaterfallSlot
+  }
+}
 </script>
